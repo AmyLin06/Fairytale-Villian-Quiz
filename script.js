0782e46bf5c0ade5a3254dce28d51dace2cb1415
AmyLin06 <43828844+AmyLin06@users.noreply.github.com>
2019-03-06 14:01:22 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+$("button").click(function(){
+    var answer1 = $(".Q1").val();
+    var answer2 = $(".Q2").val();
+    
+    if (answer1 === "power and wealth" && answer2 === "100") {
+        $(".reveal").text("The Evil Stepmother from Cinderella");
+        $(".Cinderella").show();
+    }
+    else if (answer1 === "power and wealth" && answer2 === "0") {
+        $(".reveal").text("Maleficent from Sleeping Beauty");
+        $(".Maleficent").show();
+    }
+    else if (answer1 === "manipulation and witchcraft" && answer2 === "100") {
+        $(".reveal").text("The Evil Stepmother from Snow White");
+        $(".Snow White").show();
+    }
+    else {
+        $(".reveal").text("The Witch from Hansel and Gretal");
+        $(".Witch").show();
+    }
+});
+
+
+        
+        
